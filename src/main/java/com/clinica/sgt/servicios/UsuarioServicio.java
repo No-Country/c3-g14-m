9a761b55106b71dbec685cdd35d54181c101989c
Cristian Quevedo <cristian_quevedo@hotmail.com>
2022-02-17 12:12:50 -0300

    CRUD completo de usuario en ServicioUsuario

--- src/main/java/com/clinica/sgt/servicios/UsuarioServicio.java
+++ src/main/java/com/clinica/sgt/servicios/UsuarioServicio.java
@@ -15,16 +15,16 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UsuarioServicio {
-    
+
     @Autowired
     private UsuarioRepositorio usuarioRepositorio;
-    
-     //****************************CREACION******************
+
+    //****************************CREACION******************
     @Transactional
     public void crearUsuario(String dni, String mail, String password,
             String nombreCompleto, String telefono, Genero genero, Boolean alta,
-            UserType userType){
-        
+            UserType userType) {
+
         Usuario usuario = new Usuario();
         usuario.setNombreCompleto(nombreCompleto);
         usuario.setDni(dni);
@@ -33,16 +33,83 @@ public class UsuarioServicio {
         usuario.setTelefono(telefono);
         usuario.setGenero(genero);
         usuario.setAlta(true);
-    }
-    
-    
-    //******************UPDATE***********************
-    @Transactional
-    public void modificarUsuario(String dni, String mail, String password,
-            String nombreCompleto, String telefono, Genero genero, Boolean alta,
-            UserType userType){
         
+        usuarioRepositorio.save(usuario);
     }
 
-            
+    //******************UPDATE***********************
+    @Transactional
+    public void modificarUsuario(String idUsuario,String dni, String mail, String password,
+            String nombreCompleto, String telefono, Genero genero, Boolean alta,
+            UserType userType) {
+        Usuario usuario = usuarioRepositorio.buscarPorID(idUsuario);
+        
+        usuario.setNombreCompleto(nombreCompleto);
+        usuario.setDni(dni);
+        usuario.setMail(mail);
+        usuario.setPassword(password);
+        usuario.setTelefono(telefono);
+        usuario.setGenero(genero);
+        usuario.setAlta(true);
+        
+        usuarioRepositorio.save(usuario);
+
+    }
+    
+     //************************BUSQUEDA O CONSULTA*******************
+    @Transactional
+    public Usuario buscarUsuarioDNI(String dni){
+       Usuario existeUsuario = usuarioRepositorio.buscarPorDNI(dni);
+       if(existeUsuario !=null){
+           return existeUsuario;
+       }
+       return null;
+    }
+    
+    @Transactional
+    public Usuario buscarUsuarioID(String idUsuario){
+       Usuario existeUsuario = usuarioRepositorio.buscarPorID(idUsuario);
+       if(existeUsuario !=null){
+           return existeUsuario;
+       }
+       return null;
+    }
+    
+    //***********************BAJA*****************
+    @Transactional
+    public void bajaUsuario(String idUsuario, UserType userType) {
+        Usuario usuario = usuarioRepositorio.buscarPorID(idUsuario);
+        switch (userType) {
+            case ADMIN:
+                if (usuario != null) {
+                    usuario.setAlta(false);
+                    usuarioRepositorio.save(usuario);
+                }
+                System.out.println("No existe un usuario con el ID ingresado");
+                break;
+            default:
+                System.out.println("No tiene privilegios de Administrador");
+                break;
+        }
+
+    }
+
+    //***********************AlTA*****************
+    @Transactional
+    public void altaUsuario(String idUsuario, UserType userType) {
+        Usuario usuario = usuarioRepositorio.buscarPorID(idUsuario);
+        switch (userType) {
+            case ADMIN:
+                if (usuario != null) {
+                    usuario.setAlta(true);
+                    usuarioRepositorio.save(usuario);
+                }
+                System.out.println("No existe un usuario con el ID ingresado");
+                break;
+            default:
+                System.out.println("No tiene privilegios de Administrador");
+                break;
+        }
+    }
+
 }
