4072a3bd00c7e2f0b3870ffe78a774e4aa90a2dd
julianRosales <rjulianbenjamin@gmail.com>
2022-02-23 09:23:21 -0300

    Arreglos provisionales en TurnoRepositorio - Funcionando

--- src/main/java/com/clinica/sgt/repositorios/TurnoRepositorio.java
+++ src/main/java/com/clinica/sgt/repositorios/TurnoRepositorio.java
@@ -2,7 +2,7 @@ package com.clinica.sgt.repositorios;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
+import java.util.List;
 
 import com.clinica.sgt.entidades.Turno;
 
@@ -14,19 +14,19 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TurnoRepositorio extends JpaRepository<Turno, String>{
     
-    @Query("SELECT t FROM Turnos t WHERE t.paciente.dni = :dni")
-    public ArrayList<Turno> buscarTurnoPaciente(@Param("dni") String dni);
+    @Query("SELECT t FROM Turno t WHERE t.paciente.dni = :dni")
+    public List<Turno> buscarTurnoPaciente(@Param("dni") String dni);
 
-    @Query("SELECT t FROM Turnos t WHERE t.admin.dni = :dni")
-    public ArrayList<Turno> buscarTurnoProfesional(@Param("dni") String dni);
+    @Query("SELECT t FROM Turno t WHERE t.personal.dni = :dni")
+    public List<Turno> buscarTurnoProfesional(@Param("dni") String dni);
 
-    @Query("SELECT t FROM Turnos t WHERE t.hora = :hora , t.dia = :dia AND t.profesional.dni = :dniProfesional")
-    public Turno buscarPorHoraYDia(@Param("hora") LocalTime hora,@Param("dia") LocalDate dia, @Param("dniProfesional") String dniProfesional);
+    // @Query("SELECT t FROM Turno t WHERE t.hora = :hora , t.dia = :dia AND t.profesional.dni = :dniProfesional")
+    // public Turno buscarPorHoraYDia(@Param("hora") LocalTime hora,@Param("dia") LocalDate dia, @Param("dniProfesional") String dniProfesional);
 
-    @Query("SELECT t FROM Turnos t WHERE t.dia = :dia AND t.profesional.dni = :dniProfesional")
-    public ArrayList<Turno> buscarPorDia(@Param("dia") LocalDate dia, @Param("dniProfesional") String dniProfesional);
+    // @Query("SELECT t FROM Turno t WHERE t.dia = :dia AND t.profesional.dni = :dniProfesional")
+    // public List<Turno> buscarPorDia(@Param("dia") LocalDate dia, @Param("dniProfesional") String dniProfesional);
 
-    @Query("SELECT t FROM Turnos t WHERE t.id = :id")
+    @Query("SELECT t FROM Turno t WHERE t.id = :id")
     public Turno buscarPorID(@Param("id") String id);
 
 
